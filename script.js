7eaab1859ad77ebc44318a4bd1b7a8053f892ef2
Mohd Asad Naseem <87413907+AsadGuru@users.noreply.github.com>
2024-09-16 01:14:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+function updateBackground() {
+	if(window.innerHeight > window.innerWidth) {
+		//portrait mode
+
+		document.body.classList.add('portrait-background');
+		document.body.classList.remove('landscape-background');		
+	} else {
+		//landscape mode
+		portrait.body.classList.add('landscape-background');
+		portrait.body.classList.remove('portrait-background');
+	}
+}
+
+//initial calll to set the background on page load
+updateBackground();
+
+//addd event listener to update background on resize
+window.addEventListener('resize', updateBackground);
